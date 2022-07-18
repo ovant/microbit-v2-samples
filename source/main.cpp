4dd5e578f95ab704db81dc8de448dd0aa6ebaea4
Ovi <ovidiu.antal36@gmail.com>
2022-07-18 14:48:09 +0100

    tested ble

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,45 @@
 #include "MicroBit.h"
+#include "Tests.h"
 
 MicroBit uBit;
+
+MicroBitBLEManager *BLEManager;
+
+
+void pressedA(MicroBitEvent)
+{
+    uBit.display.print("Aa");
+}
+
+void pressedB(MicroBitEvent)
+{
+    uBit.display.print("Bb");
+}
+
+void pressedAB(MicroBitEvent)
+{
+    uBit.display.print("Cc");
+}
+
+void buttons(){
+    uBit.messageBus.listen(DEVICE_ID_BUTTON_A, DEVICE_BUTTON_EVT_CLICK, pressedA);
+    uBit.messageBus.listen(DEVICE_ID_BUTTON_B, DEVICE_BUTTON_EVT_CLICK, pressedB);
+    uBit.messageBus.listen(DEVICE_ID_BUTTON_AB, DEVICE_BUTTON_EVT_CLICK, pressedAB);
+
+    while(1)
+        uBit.sleep(1000);
+}
 
 int 
 main()
 {
     uBit.init();
+    // BLEManager->pairingMode(uBit.display,uBit.buttonA);
+    // BLEManager->showNameHistogram(uBit.display);
 
-    while(1)
-        uBit.display.scroll("HELLO WORLD!");
+    // temperature_test();
+    ble_test();
+    buttons();
+    // while(1)
+    //     uBit.display.scroll("HELLO WORLD!");
 }
