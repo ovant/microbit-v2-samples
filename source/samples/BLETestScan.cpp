91ab3710f42dcaf5644e0f632b680c4a58e6344c
Ovi <ovidiu.antal36@gmail.com>
2022-08-17 13:02:01 +0100

    more testing features

--- source/samples/BLETestScan.cpp
+++ source/samples/BLETestScan.cpp
@@ -1,17 +1,36 @@
 #include "MicroBit.h"
 #include "Tests.h"
+#include <string>
 
 extern MicroBit uBit;
 
 void ble_test_scan()
 {
 
+    bool pfoai;
+    uBit.display.setBrightness(70);
+
     uBit.display.scroll("B");
+
+    uBit.ble->initializeScan();
 
     uBit.ble->startScanning();
 
     uBit.display.scroll('S');
 
+
+    while(1){
+        uBit.display.print("s");
+        uBit.sleep(500);
+        uBit.display.clear();
+        uBit.sleep(500);
+        pfoai = uBit.ble->getFlag();
+        // if(uBit.buttonA.isPressed())
+        //     uBit.ble->setFlag(true);
+        if(pfoai){
+            uBit.display.scroll("yey");
+        }
+    }
     
 }
 
