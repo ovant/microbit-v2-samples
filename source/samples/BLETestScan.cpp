0435db46f18b8333c0df42b47184fea3bc53b50b
Ovi <ovidiu.antal36@gmail.com>
2022-08-22 15:21:27 +0100

    testing oop

--- source/samples/BLETestScan.cpp
+++ source/samples/BLETestScan.cpp
@@ -4,10 +4,19 @@
 
 extern MicroBit uBit;
 
+void display_number(uint8_t n){
+    if(n>25)
+        n = 25;
+    for(uint8_t i=0;i<n;i++){
+        uBit.display.image.setPixelValue(i%5,i/5,200);
+    }
+}
+
+
 void ble_test_scan()
 {
 
-    bool pfoai;
+    bool scan_flag;
     uBit.display.setBrightness(70);
 
     uBit.display.scroll("B");
@@ -19,17 +28,21 @@ void ble_test_scan()
     uBit.display.scroll('S');
 
 
+
     while(1){
         uBit.display.print("s");
-        uBit.sleep(500);
+        uBit.sleep(250);
         uBit.display.clear();
-        uBit.sleep(500);
-        pfoai = uBit.ble->getFlag();
-        // if(uBit.buttonA.isPressed())
-        //     uBit.ble->setFlag(true);
-        if(pfoai){
-            uBit.display.scroll("yey");
+        scan_flag = uBit.ble->getFlag();
+        if(scan_flag){
+
+            LightricityData data = uBit.ble->getScanResults();
+            unsigned int lux = data.getLux();
+            // if(lux)               
+            //     while(1)
+            //         uBit.display.scroll(std::to_string(lux));
         }
+        uBit.sleep(250);
     }
     
 }
